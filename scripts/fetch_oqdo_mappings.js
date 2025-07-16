f838f9a36113b218fcbda0fd795e0728c5fdb248
Andreas Scharf <admin@andreas-scharf.at>
2025-07-16 09:23:09 +0200

    generalized oqdo fetch script

--- scripts/fetch_oqdo_mappings.js
+++ scripts/fetch_oqdo_mappings.js
@@ -3,106 +3,117 @@ const fs = require("fs").promises;
 const OQDO_BASE_URL = "https://oqdo-apim-prod.azure-api.net";
 const OQDO_TOKEN = process.env.OQDO_TOKEN || "";
 
-const getMappings = async (groupId) => {
+const DEVICE_BLACKLIST = [
+  "AutomationApps.Collections.SpaceCollections.Campus",
+  "AutomationApps.ReportsAndStatistics.Alarm.TopAlarms",
+  "AutomationApps.Devices.ExistingBacnetDevice",
+  "AutomationApps.Devices.BacnetDevice",
+  "AutomationApps.Devices.EdgeDevice",
+  "AutomationApps.Plants.Buildings.P_Building",
+  "AutomationApps.Plants.Buildings.P_Floor",
+  "AutomationApps.Plants.AirHandlingUnits.P_AirHandlingUnit",
+  "AutomationApps.Plants.Heatings.P_HeatingPuffer",
+  "AutomationApps.Plants.Heatings.P_HeatingOutlet",
+  "AutomationApps.Plants.Commons.P_Common",
+  "AutomationApps.Plants.Heatings.P_HeatingBoiler",
+  "AutomationApps.Plants.Heatings.P_DistrictHeating",
+];
+
+const VALUE_WHITELIST = [
+  "value",
+  "isOn",
+  "flow1",
+  "flow2",
+  "flowMax",
+  "flowMin",
+  "flowBase",
+  "outT1",
+  "outT2",
+];
+
+const getMappings = async (resourceId, groupId = null) => {
   const mappings = {};
 
   try {
-    const group = await getGroup(groupId);
-
-    console.log("Analyizing WiFI group:", group.name);
-
-    const buildings = await getResource(
-      group.resourceGroupId,
-      group.id,
-      "AutomationApps.Plants.Buildings.P_Building"
-    );
+    const devices = await analyize(resourceId, groupId);
+    const converted = convert(resourceId, devices);
 
     console.log(
-      "Found buildings:",
-      buildings.length,
-      buildings.map((b) => b.name)
+      converted.length,
+      "devices found for group:",
+      resourceId,
+      groupId
     );
 
-    for (const building of buildings) {
-      const sections = await getResource(
-        group.resourceGroupId,
-        building.id,
-        "AutomationApps.Plants.Buildings.P_RoomAutomation"
-      );
-
-      for (const section of sections) {
-        const stories = await getResource(
-          group.resourceGroupId,
-          section.id,
-          "AutomationApps.Plants.Buildings.P_Floor"
-        );
-
-        for (const story of stories) {
-          const rooms = await getResource(group.resourceGroupId, story.id);
-
-          for (const room of rooms) {
-            const devices = await getResource(group.resourceGroupId, room.id);
-
-            if (devices.length > 0) {
-              devices.forEach((device) => {
-                mappings[`${group.resourceGroupId}.${device.id}`] =
-                  convertMappingKey(building, section, story, room, device);
-              });
-            }
-          }
-        }
-      }
+    for (const device of converted) {
+      mappings[device.key] = device.name;
     }
   } catch (e) {
-    console.error("Error fetching mappings:", e, groupId);
+    console.error("Error fetching mappings:", e, resourceId, groupId);
   }
 
   return mappings;
 };
 
-const convertMappingKey = (building, section, story, room, device) => {
+const analyize = async (parent, child = null) => {
+  const devices = [];
+
   try {
-    const buildingName = building.name
-      .split(/\s+/)
-      .map((w) => w[0])
-      .join("");
+    const url = [parent, child || "", !!child ? "children" : ""]
+      .filter(Boolean)
+      .join("/");
 
-    const roomName = room.name.replace(/\D/g, "");
-    const deviceName = device.name.replace(/[^a-zA-Z0-9]/g, "");
+    const data = await makeRequest(url);
 
-    return `${buildingName}.${story.name}${roomName}.${deviceName}.value`;
-  } catch (e) {
-    console.error("Error converting mapping key:", e);
-    return null;
-  }
+    if (data && Array.isArray(data)) {
+      for (const device of data) {
+        if (!DEVICE_BLACKLIST.includes(device.schema)) {
+          console.log(`Found device: ${device.name} - ${device.id}`);
+          devices.push(device);
+        }
+
+        const children = await analyize(parent, device.id);
+        devices.push(...children);
+      }
+    }
+  } catch (e) {}
+
+  return devices;
 };
 
-const getGroup = async (groupId) => {
-  const data = await makeRequest(groupId);
+const convert = (groupId, devices) => {
+  const results = [];
 
-  return data
-    .filter(
-      (d) => d.schema === "AutomationApps.Collections.SpaceCollections.Campus"
-    )
-    .pop();
-};
+  for (const device of devices) {
+    const keys = Object.keys(device).filter((key) =>
+      VALUE_WHITELIST.includes(key)
+    );
 
-const getResource = async (groupId, resourceId, schemaFilter) => {
-  const data = await makeRequest(`${groupId}/${resourceId}/children`);
+    const newDevices = keys.map((k) => {
+      const keysParts = [groupId, device.id, k];
+      const key = keysParts.join(".");
 
-  return data
-    .map((device) => {
+      const deviceName =
+        device.externalIds?.PlantIdentification ||
+        device.name + "." + device.description ||
+        "";
+
+      const fixedName = deviceName
+        .replace(/'/g, ".")
+        .replace(/\s+/g, "")
+        .trim();
+
+      const name = `${fixedName}.${k}${k === "value" ? "" : ".value"}`;
+
       return {
-        id: device.id,
-        name: device.name,
-        schema: device.schema,
-        device,
+        key,
+        name,
       };
-    })
-    .filter((device) => {
-      if (schemaFilter) return device.schema === schemaFilter;
-      return true;
     });
+    results.push(...newDevices);
+  }
+
+  return results;
 };
 
 const makeRequest = async (url) => {
@@ -123,8 +134,22 @@ const makeRequest = async (url) => {
 };
 
 (async () => {
-  const project = "232ae843-68c3-42e3-ba79-a486a188724a"; // WIFI-Linz
-  const mappings = await getMappings(project);
+  const resourceId = process.argv[2];
+  const groupId = process.argv[3];
 
-  await fs.writeFile("import_mappings.json", JSON.stringify(mappings, null, 2));
+  if (!resourceId) {
+    console.log(
+      "Usage: node fetch_oqdo_mappings.js <resourceId> <groupId (optional)>"
+    );
+    return;
+  }
+
+  console.log("Fetching oqdo mappings for:", resourceId, groupId);
+
+  const mappings = await getMappings(resourceId, groupId);
+
+  await fs.writeFile(
+    "import_mappings.json",
+    JSON.stringify({ oqdo: mappings }, null, 2)
+  );
 })();
